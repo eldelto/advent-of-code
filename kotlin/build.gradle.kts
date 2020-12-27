398f600ec8b2dc123294df18ab29c90931fa8401
eldelto <eldelto77@gmail.com>
2020-12-27 22:41:20 +0100

    2020 day 19 part 2 Kotlin version

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,14 +1,20 @@
 plugins {
-    kotlin("jvm") version "1.4.20"
+  kotlin("jvm") version "1.4.20"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+  implementation(kotlin("stdlib"))
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.3")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.3")
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
 }
