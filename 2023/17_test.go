72aafcbb39995f760ac8524a9346614751e20202
Dominic Aschauer <dominic.aschauer@radancy.com>
2023-12-17 19:10:45 +0100

    2023 day 17 part 1 remove unneeded code

--- 2023/17_test.go
+++ 2023/17_test.go
@@ -92,7 +92,7 @@ func aStar2(matrix Matrix[int]) crucible {
 		closed[k] = current
 
 		possiblePaths := possiblePaths(matrix, current)
-		fmt.Println(current)
+		// fmt.Println(current)
 		// fmt.Println(possiblePaths)
 		// fmt.Println()
 		for _, path := range possiblePaths {
@@ -119,7 +119,7 @@ func aStar2(matrix Matrix[int]) crucible {
 				direction: newOpenNode.direction,
 				stepCount: newOpenNode.stepCount,
 			}
-			if _, ok := closed[k]; ok { //&& newOpenNode.heatLoss-5 > other.heatLoss {
+			if _, ok := closed[k]; ok {
 				// fmt.Println("skip")
 				continue
 			}
@@ -127,8 +127,6 @@ func aStar2(matrix Matrix[int]) crucible {
 			heap.Push(&open, &crucibleItem{crucible: newOpenNode, estimatedCost: estimatedCost})
 		}
 
-		// newCurrentItem, _ := heap.Pop(&open).(*crucibleItem)
-		// current = newCurrentItem.crucible
 		for {
 			newCurrentItem, _ := heap.Pop(&open).(*crucibleItem)
 			c := newCurrentItem.crucible
@@ -137,7 +135,7 @@ func aStar2(matrix Matrix[int]) crucible {
 				direction: c.direction,
 				stepCount: c.stepCount,
 			}
-			if _, ok := closed[k]; ok { //&& c.heatLoss-5 > other.heatLoss {
+			if _, ok := closed[k]; ok {
 				continue
 			}
 
@@ -147,164 +145,6 @@ func aStar2(matrix Matrix[int]) crucible {
 	}
 
 	return current
-}
-
-type aStarNode struct {
-	pos           Vec2
-	traversalCost int
-	estimatedCost int
-	parent        *aStarNode
-	index         int
-}
-
-func (n aStarNode) Priority() int {
-	return n.estimatedCost
-}
-
-func (n aStarNode) Index() int {
-	return n.index
-}
-
-func (n aStarNode) SetIndex(i int) {
-	n.index = i
-}
-
-func aStarHeuristic(current, target Vec2) uint {
-	// return current.ManhattenDistance(target)
-	return target.Sub(current).Len()
-}
-
-func possibleNodes(matrix Matrix[int], current aStarNode) []Vec2 {
-	positions := []Vec2{
-		current.pos.Add(Vec2(North)),
-		current.pos.Add(Vec2(East)),
-		current.pos.Add(Vec2(South)),
-		current.pos.Add(Vec2(West)),
-	}
-
-	if current.parent != nil {
-		dst := &current
-		src := dst.parent
-		previousDirection := DirectionFromPositions(src.pos, dst.pos)
-		straightCount := 1
-		for i := 0; i < 2; i++ {
-			if src.parent == nil {
-				break
-			}
-
-			dst = src
-			src = dst.parent
-			direction := DirectionFromPositions(src.pos, dst.pos)
-			if direction != previousDirection {
-				break
-			}
-			previousDirection = direction
-			straightCount++
-		}
-
-		// Need to turn
-		if straightCount >= 3 {
-			positions = []Vec2{
-				current.pos.Add(Vec2(previousDirection.Left())),
-				current.pos.Add(Vec2(previousDirection.Right())),
-			}
-		}
-	}
-
-	return Filter(positions, func(pos Vec2) bool {
-		return matrix.WithinBounds(pos)
-	})
-}
-
-type key struct {
-	pos Vec2
-	// parent Vec2
-}
-
-func aStar(matrix Matrix[int]) aStarNode {
-	current := aStarNode{pos: Vec2{0, 0}}
-	target := Vec2{len(matrix) - 1, len(matrix[0]) - 1}
-
-	// open := []aStarNode{}
-	open := PriorityQueue[aStarNode]{}
-	closed := map[key]struct{}{} //map[Vec2]aStarNode{current.pos: current}
-
-	for current.pos != target {
-		// fmt.Println(current.pos)
-		k := key{pos: current.pos}
-		// if current.parent != nil {
-		// 	k.parent = current.parent.pos
-		// }
-		closed[k] = struct{}{} //current
-
-		possibleNodes := possibleNodes(matrix, current)
-		for _, possibleNode := range possibleNodes {
-			k := key{pos: possibleNode} //, parent: current.pos}
-			if _, ok := closed[k]; ok {
-				// fmt.Println("skip")
-				continue
-			}
-
-			traversalCost := current.traversalCost + matrix.Get(possibleNode)
-			estimatedCost := int(aStarHeuristic(possibleNode, target)) + traversalCost
-			parent := current
-			newOpenNode := aStarNode{
-				pos:           possibleNode,
-				traversalCost: traversalCost,
-				estimatedCost: int(estimatedCost),
-				parent:        &parent}
-
-			// open = append(open, newOpenNode)
-			heap.Push(&open, newOpenNode)
-			// slices.SortFunc(open, func(a, b aStarNode) int {
-			// 	return a.estimatedCost - b.estimatedCost
-			// })
-		}
-
-		newCurrent, _ := heap.Pop(&open).(aStarNode)
-		current = newCurrent
-		// open = open[1:]
-		// for i := 0; i < len(open); i++ {
-		// 	next := open[i]
-		// 	if _, ok := closed[next.pos]; !ok {
-		// 		current = next
-		// 		break
-		// 	}
-
-		// 	if i == len(open)-1 {
-		// 		panic("no possible node found")
-		// 	}
-		// }
-	}
-
-	return current
-}
-
-func nodeContainsPos(node aStarNode, pos Vec2) bool {
-	if node.pos == pos {
-		return true
-	}
-
-	if node.parent != nil {
-		return nodeContainsPos(*node.parent, pos)
-	}
-
-	return false
-}
-
-func printPath(m Matrix[int], node aStarNode) {
-	for ri, row := range m {
-		for ci := range row {
-			pos := Vec2{ci, ri}
-			value := m.Get(pos)
-			if nodeContainsPos(node, pos) {
-				fmt.Print("*")
-			} else {
-				fmt.Print(value)
-			}
-		}
-		fmt.Println()
-	}
 }
 
 func crucibleTraversedPos(node crucible, pos Vec2) bool {
