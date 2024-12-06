932e4d2c598bfb4b5a7b839faad3558fb945ca2f
eldelto <eldelto77@gmail.com>
2024-12-06 11:00:13 +0100

    2024 day 5 part 1

--- 2024/elixir/advent_of_code/lib/advent_of_code.ex
+++ 2024/elixir/advent_of_code/lib/advent_of_code.ex
@@ -39,4 +39,65 @@ defmodule AdventOfCode do
     |> Stream.map(&mul/1)
     |> Enum.sum()
   end
+
+
+  def valid_print?([page], ruleDict) do
+	true
+  end
+
+  def valid_print?([page | pagesBefore], ruleDict) do
+	pagesAfter = Map.get(ruleDict, page, [])
+
+	case Enum.find(pagesBefore, fn pageBefore ->
+	  Enum.find(pagesAfter, &(&1 == pageBefore)) != nil
+	  end) do
+	nil -> valid_print?(pagesBefore, ruleDict)
+	_ -> false
+	  end
+  end
+
+  def day5_part1 do
+    {:ok, input} = File.read("inputs/05.txt")
+
+	[rawRules, rawPrints] = String.split(input, "\n\n")
+
+	ruleDict = String.split(rawRules, "\n")
+	|> Enum.reduce(%{}, fn rule, acc ->
+	  [key, value] = String.split(rule, "|")
+	  Map.update(acc, key, [value], fn old -> [value | old] end)
+	end)
+
+	prints = String.split(rawPrints, "\n")
+	|> Stream.filter(&(&1 != ""))
+	|> Stream.map(fn line -> String.split(line, ",") end)
+	|> Enum.to_list()
+
+	Enum.filter(prints, fn print -> valid_print?(Enum.reverse(print), ruleDict) end)
+	|> Enum.map(fn print ->
+	  middle = ceil(length(print)/2 - 1)
+	  {value, _} = Integer.parse(Enum.at(print, middle))
+	  value
+	  end)
+	|> Enum.sum
+  end
+
+  def day5_part2 do
+    {:ok, input} = File.read("inputs/05-test.txt")
+
+	[rawRules, rawPrints] = String.split(input, "\n\n")
+
+	ruleDict = String.split(rawRules, "\n")
+	|> Enum.reduce(%{}, fn rule, acc ->
+	  [key, value] = String.split(rule, "|")
+	  Map.update(acc, key, [value], fn old -> [value | old] end)
+	end)
+
+	prints = String.split(rawPrints, "\n")
+	|> Stream.filter(&(&1 != ""))
+	|> Stream.map(fn line -> String.split(line, ",") end)
+	|> Enum.to_list()
+
+	Enum.filter(prints, fn print -> !valid_print?(Enum.reverse(print), ruleDict) end)
+	#TODO
+  end
 end
